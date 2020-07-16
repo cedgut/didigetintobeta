384e69405a1af0f5f1258d872774821444f3c399
cedgut <cedricgutzeit@gmail.com>
2020-07-17 01:31:22 +0200

    Updates

---
+++ precache-manifest.0a5651c027b06a79fbc726a5d0c09ecd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c800f1970068bdfa086b4624290a09fc",
+    "url": "/didigetintobeta/index.html"
+  },
+  {
+    "revision": "5c15820b6e4cb23b7a75",
+    "url": "/didigetintobeta/static/css/main.82117b89.chunk.css"
+  },
+  {
+    "revision": "5a3e65e3e0761aa9da51",
+    "url": "/didigetintobeta/static/js/2.473ada3c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/didigetintobeta/static/js/2.473ada3c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c15820b6e4cb23b7a75",
+    "url": "/didigetintobeta/static/js/main.851350f9.chunk.js"
+  },
+  {
+    "revision": "54df08a7f780c9c33bd8",
+    "url": "/didigetintobeta/static/js/runtime-main.fc8d7e25.js"
+  },
+  {
+    "revision": "c9cd4c9a83feee0c3de32fdd29dea31f",
+    "url": "/didigetintobeta/static/media/logo_shadowlands0.c9cd4c9a.png"
+  },
+  {
+    "revision": "ceec5a01bc267797d17d74782cba079f",
+    "url": "/didigetintobeta/static/media/logo_shadowlands1.ceec5a01.png"
+  }
+]);
